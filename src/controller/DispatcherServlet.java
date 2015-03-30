efb4b827b4cdf0c509c42faa8b5fd436e746d5d2
azhurat <artur.zhurat@gmail.com>
2015-03-30 23:46:52 +0300

    add servlet 3 0 and annotations support

--- src/controller/DispatcherServlet.java
+++ src/controller/DispatcherServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class DispatcherServlet
  */
-@WebServlet("/DispatcherServlet")
+@WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
